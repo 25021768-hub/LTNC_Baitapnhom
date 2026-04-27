364587881e0e1e3528d123802b8134c7ad4a3f77
giabao848 <bdinhvugia@gmail.com>
2026-04-28 00:46:07 +0700

    Kết nối các giao diện login

--- src/main/java/com/example/onlineauctionsystem/controller/LoginController.java
+++ src/main/java/com/example/onlineauctionsystem/controller/LoginController.java
@@ -5,8 +5,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class LoginController extends BaseController{
-    @FXML
-    public Hyperlink btnGoToRegister;
+    @FXML private TextField txtUsername;
+    @FXML private PasswordField txtPassword;
+    @FXML private PasswordField txtRePassword;
+    @FXML private Label lblUsernameMessage;
+    @FXML private Label lblPasswordMessage1;
+    @FXML private Label lblPasswordMessage2;
+    @FXML private Hyperlink btnGoToRegister;
+
     @FXML
     private void showAlert(String title, String content) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -15,8 +21,43 @@ public class LoginController extends BaseController{
         alert.setContentText(content);
         alert.showAndWait();
     }
+
+    //---------- PHẦN ĐĂNG KÍ-------------
+    // Chuyển màn hình đăng kí
     @FXML
     private void onRegisterClick(ActionEvent event){
         switchScene(event, "/com/example/onlineauctionsystem/Dang_Ky.fxml", "Đăng kí tài khoản");
     }
+
+    //Chuyển về màn hình đăng nhập
+    @FXML
+    private void onReturnLogin(ActionEvent event){
+        switchScene(event, "/com/example/onlineauctionsystem/Dang_Nhap_BTL.fxml", "Đăng nhập tài khoản");
+    }
+
+    //Set label ẩn đi
+    private void setUpLabel(Label label){
+        label.setVisible(false);
+        label.setManaged(false);
+    }
+
+    //Khời tạo
+    @FXML
+    private void initialize(){
+        if (lblUsernameMessage != null) setUpLabel(lblUsernameMessage);
+        if (lblPasswordMessage1 != null) setUpLabel(lblPasswordMessage1);
+        if (lblPasswordMessage2 != null) setUpLabel(lblPasswordMessage2);
+    }
+    //---------PHẦN ĐĂNG NHẬP----------
+    @FXML
+    private void onForgotPassword(ActionEvent event){
+        switchScene(event, "/com/example/onlineauctionsystem/Quen_Mat_Khau_BTL.fxml", "Quên mật khẩu");
+    }
+
+    //--------Phần quên mật khẩu---------
+
 }
+
+
+
+
